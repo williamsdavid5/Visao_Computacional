301efd1494d64104d95ed2aff8f56249016d8e1d
David Duarte (Williams) <davidduart04@gmail.com>
2025-01-26 16:35:50 -0300

    LBPH teste de acurácia

--- 2- Reconhecimento facial/LBPH.py
+++ 2- Reconhecimento facial/LBPH.py
@@ -2,11 +2,12 @@ from PIL import Image
 import cv2
 import numpy as np
 import os
+from sklearn.metrics import accuracy_score
 
 # preparação as imagens para o treinamento
 
 
-def get_image_data():
+def get_image_data():  # a função percorre a pasta com as imagen de treino e resgata o id de cada imagem de seus pixels
     paths = [os.path.join('yalefaces\\train', f)
              for f in os.listdir('yalefaces\\train')]
     # print(paths)
@@ -27,12 +28,14 @@ def get_image_data():
 ids, faces = get_image_data()
 
 # treinamento do classificador LBPH
+# o classificador cria um arquivo que irá conter os dados para fazer o reconhecimento das pessoas
 lbph_classifier = cv2.face.LBPHFaceRecognizer_create()
 lbph_classifier.train(faces, ids)
 lbph_classifier.write('2- Reconhecimento facial//lbph_classifier.yml')
 
 
 # reconhecimento
+# se baseando no arquivo com os dados de cada face, o algoriitmo irá fazer o reconhecimento em uma imagem
 lbph_face_classifier = cv2.face.LBPHFaceRecognizer_create()
 lbph_face_classifier.read('2- Reconhecimento facial\\lbph_classifier.yml')
 
@@ -50,11 +53,36 @@ print(f"Pessoa ID: {previsao[0]}\nPessoa Esperada ID: {
 y_start = 30  # Posição inicial no eixo Y
 line_height = 20  # Espaço entre as linhas
 
-cv2.putText(imagem_np, f"Pred: {
-            previsao[0]}", (10, y_start), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 1)
-cv2.putText(imagem_np, f"Exp: {saida_esperada}", (
-    10, y_start + line_height), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 1)
+# resultado do reconhecimento em uma imagem
+# cv2.putText(imagem_np, f"Pred: {
+#             previsao[0]}", (10, y_start), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 1)
+# cv2.putText(imagem_np, f"Exp: {saida_esperada}", (
+#     10, y_start + line_height), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 1)
+# cv2.imshow(f"Confidence: {previsao[1]:.2f}", imagem_np)
+# cv2.waitKey(0)
 
+# avaliação do classificador
+# separa os ids existentes na pasta e faz as previsões para cada imagem
+# assim poderiamos comparar os resultados e medir a acuracia
+paths = [os.path.join('yalefaces\\test', f)
+         for f in os.listdir('yalefaces\\test')]
+previsoes = []
+saidas_esperadas = []
 
-cv2.imshow(f"Confidence: {previsao[1]:.2f}", imagem_np)
-cv2.waitKey(0)
+for path in paths:
+    imagem = Image.open(path).convert('L')
+    imagem_np = np.array(imagem, 'uint8')
+    previsao, _ = lbph_face_classifier.predict(imagem_np)
+    saida_esperada = int(os.path.split(path)[1].split('.')[
+                         0].replace('subject', ''))
+    # print(saida_esperada)
+
+    previsoes.append(previsao)
+    saidas_esperadas.append(saida_esperada)
+
+previsoes = np.array(previsoes)
+saidas_esperadas = np.array(saidas_esperadas)
+
+# print(previsoes)
+# print(saidas_esperadas)
+print(f'Acuracia: {100*accuracy_score(saidas_esperadas, previsoes)} %')
